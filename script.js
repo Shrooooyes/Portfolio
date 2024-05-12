de773066d27d78c34443d3c9a55f706d7e033e35
ShreyashSawant <112112961+Shrooooyes@users.noreply.github.com>
2024-05-12 12:54:59 +0530

    Add files via upload
    Added hamburger menu and some media queries

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const sendEmail = () => {
     let url = "https://mail.google.com/mail/?view=cm&fs=1&to=shreyashsawant37@gmail.com";
     window.location.href = url;
 }
+
+const openNav = () => {
+    document.getElementById("item2").style.width = "50%";
+}
+
+const closeNav = () => {
+    document.getElementById("item2").style.width = "0%";
+}
